daec73ed3ade3a76b07ce5a04c853e709245f05e
Nivea Stelmam <mand458@hotmail.com>
2025-11-14 17:03:46 -0300

    validation

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,30 @@
 const myModal = new bootstrap.Modal("#register-modal");
 
+// Login to the system
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const session = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account){
+        alert("Ops! Verifique o usuário ou a senha.");
+        return;
+    }
+
+    if(account){
+        if(account.password !== password){
+            alert("Ops! Verifique o usuário ou a senha.");
+            return;
+        }
+
+        window.location.href = "home.html";
+    }
+});
+
 // Create Account 
 document.getElementById("create-form").addEventListener("submit", function (e) {
   e.preventDefault();
@@ -33,4 +58,13 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
 // localStorage - Allows you to save key-value pairs in the browser.
 function saveAccount(data){
     localStorage.setItem(data.login, JSON.stringify(data)); // converts a JavaScript object into a JSON string
+}
+
+function getAccount(key){
+    const account = localStorage.getItem(key);
+
+    if(account){
+        return JSON.parse(account); // converts a JSON string into a JavaScript object
+    }
+    return "";
 }
