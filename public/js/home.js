d30331f5f4bee8bdf181896eec99ae2f1c521685
Nivea Stelmam <mand458@hotmail.com>
2025-11-21 15:16:00 -0300

    start of transactions

---
+++ public/js/home.js
@@ -0,0 +1,43 @@
+const myModal = new bootstrap.Modal("#transaction-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+let data = {
+     transactions: []
+};
+
+document.getElementById("button-logout").addEventListener("click", logout);
+
+// Add releases
+document.getElementById("transaction-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+})
+
+
+
+checkLogged();
+
+function checkLogged(){
+    if(session){
+       sessionStorage.getItem("logged", session);
+       logged = session;
+    }
+    if(!logged){
+             window.location.href = "index.html";
+    }
+    
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser){
+        data = JSON.parse(dataUser);
+    }
+    console.log(data);
+}
+
+function logout(){
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+}
+
