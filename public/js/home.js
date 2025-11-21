c9660b24102720a9943fb073d079bc94cf5fbf0b
Nivea Stelmam <mand458@hotmail.com>
2025-11-21 19:13:25 -0300

    bottom margin and indentation

--- public/js/home.js
+++ public/js/home.js
@@ -3,87 +3,91 @@ let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
 let data = {
-     transactions: []
+  transactions: [],
 };
 
 document.getElementById("button-logout").addEventListener("click", logout);
-document.getElementById("transactions-button").addEventListener("click", function(){
+document
+  .getElementById("transactions-button")
+  .addEventListener("click", function () {
     window.location.href = "transactions.html";
-});
+  });
 
 // Add releases
-document.getElementById("transaction-form").addEventListener("submit", function(e){
+document
+  .getElementById("transaction-form")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
 
     const value = parseFloat(document.getElementById("value-input").value);
     const description = document.getElementById("description-input").value;
     const date = document.getElementById("date-input").value;
-    const type = document.querySelector('input[name="type-input"]:checked').value;
+    const type = document.querySelector(
+      'input[name="type-input"]:checked'
+    ).value;
 
     data.transactions.unshift({
-        value: value,
-        type: type,
-        description: description,
-        date: date
+      value: value,
+      type: type,
+      description: description,
+      date: date,
     });
 
     saveData(data);
     e.target.reset();
     myModal.hide();
-    
+
     getCashIn();
     getCashOut();
     getTotal();
 
     alert("Lançamento adicionado com sucesso!");
-});
-
-
+  });
 
 checkLogged();
 
-function checkLogged(){
-    if(session){
-       sessionStorage.getItem("logged", session);
-       logged = session;
-    }
-    if(!logged){
-             window.location.href = "index.html";
-    }
-    
-    const dataUser = localStorage.getItem(logged);
-    if(dataUser){
-        data = JSON.parse(dataUser);
-    }
-    getCashIn();
-    getCashOut();
-    getTotal();
-}
-
-function logout(){
-    sessionStorage.removeItem("logged");
-    localStorage.removeItem("session");
-
+function checkLogged() {
+  if (session) {
+    sessionStorage.getItem("logged", session);
+    logged = session;
+  }
+  if (!logged) {
     window.location.href = "index.html";
+  }
+
+  const dataUser = localStorage.getItem(logged);
+  if (dataUser) {
+    data = JSON.parse(dataUser);
+  }
+  getCashIn();
+  getCashOut();
+  getTotal();
 }
 
-function getCashIn(){
-    const transactions = data.transactions;
+function logout() {
+  sessionStorage.removeItem("logged");
+  localStorage.removeItem("session");
 
-    const cashIn = transactions.filter((item) => item.type === "1");
-    
-    if(cashIn.length){
-        let cashInHtml = ``;
-        let limit = 0;
+  window.location.href = "index.html";
+}
 
-        if(cashIn.length > 5){
-            limit = 5;
-        } else {
-            limit = cashIn.length;
-        }
+function getCashIn() {
+  const transactions = data.transactions;
 
-        for (let index = 0; index < limit; index++) {
-            cashInHtml += `
+  const cashIn = transactions.filter((item) => item.type === "1");
+
+  if (cashIn.length) {
+    let cashInHtml = ``;
+    let limit = 0;
+
+    if (cashIn.length > 5) {
+      limit = 5;
+    } else {
+      limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+      cashInHtml += `
             <div class="row mb-4">
                 <div class="col-12">
                     <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
@@ -99,30 +103,29 @@ function getCashIn(){
                     </div>
                </div>
            </div>
-             `
-
-        }
-        document.getElementById("cash-in-list").innerHTML = cashInHtml;
+             `;
     }
+    document.getElementById("cash-in-list").innerHTML = cashInHtml;
+  }
 }
 
-function getCashOut(){
-    const transactions = data.transactions;
+function getCashOut() {
+  const transactions = data.transactions;
 
-    const cashIn = transactions.filter((item) => item.type === "2");
-    
-    if(cashIn.length){
-        let cashInHtml = ``;
-        let limit = 0;
+  const cashIn = transactions.filter((item) => item.type === "2");
 
-        if(cashIn.length > 5){
-            limit = 5;
-        } else {
-            limit = cashIn.length;
-        }
+  if (cashIn.length) {
+    let cashInHtml = ``;
+    let limit = 0;
 
-        for (let index = 0; index < limit; index++) {
-            cashInHtml += `
+    if (cashIn.length > 5) {
+      limit = 5;
+    } else {
+      limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+      cashInHtml += `
             <div class="row mb-4">
                 <div class="col-12">
                     <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
@@ -138,33 +141,27 @@ function getCashOut(){
                     </div>
                </div>
            </div>
-             `
-
-        }
-        document.getElementById("cash-out-list").innerHTML = cashInHtml;
+             `;
     }
+    document.getElementById("cash-out-list").innerHTML = cashInHtml;
+  }
 }
-
 
 function getTotal() {
-    const transactions = data.transactions;
-    let total = 0;
+  const transactions = data.transactions;
+  let total = 0;
 
-    transactions.forEach((item) => {
-        if(item.type === "1"){
-            total += item.value;
+  transactions.forEach((item) => {
+    if (item.type === "1") {
+      total += item.value;
     } else {
-        total -= item.value;
+      total -= item.value;
     }
+  });
 
-});
-
-    document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
+  document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
 }
 
-
-
-function saveData(data){
-    localStorage.setItem(data.login, JSON.stringify(data));
+function saveData(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
 }
-
--- public/js/index.js
+++ public/js/index.js
@@ -6,90 +6,87 @@ checkLogged();
 
 // Login to the system
 document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const email = document.getElementById("email-input").value;
-    const password = document.getElementById("password-input").value;
-    const checkSession = document.getElementById("session-check").checked;
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const checkSession = document.getElementById("session-check").checked;
 
-    const account = getAccount(email);
+  const account = getAccount(email);
 
-    if(!account){
-        alert("Ops! Verifique o usuário ou a senha.");
-        return;
+  if (!account) {
+    alert("Ops! Verifique o usuário ou a senha.");
+    return;
+  }
+
+  if (account) {
+    if (account.password !== password) {
+      alert("Ops! Verifique o usuário ou a senha.");
+      return;
     }
 
-    if(account){
-        if(account.password !== password){
-            alert("Ops! Verifique o usuário ou a senha.");
-            return;
-        }
-
-        saveSession(email, checkSession);
-        window.location.href = "home.html";
-    }
+    saveSession(email, checkSession);
+    window.location.href = "home.html";
+  }
 });
 
-// Create Account 
+// Create Account
 document.getElementById("create-form").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const email = document.getElementById("email-create-input").value;
   const password = document.getElementById("password-create-input").value;
 
-  if(email.length < 5){
+  if (email.length < 5) {
     alert("Preencha o campo com um E-mail válido");
     return;
   }
 
-  if(password.length < 4){
+  if (password.length < 4) {
     alert("A senha deve ter no mínimo 4 caracteres");
     return;
   }
 
-
   saveAccount({
     login: email,
     password: password,
-    transactions: []
+    transactions: [],
   });
 
   myModal.hide();
   alert("Conta criada com sucesso!");
-
 });
 
-function checkLogged(){
-    if(session){
-       sessionStorage.getItem("logged", session);
-       logged = session;
-    }
-    if(logged){
-       saveSession(logged, session);
+function checkLogged() {
+  if (session) {
+    sessionStorage.getItem("logged", session);
+    logged = session;
+  }
+  if (logged) {
+    saveSession(logged, session);
 
-       window.location.href = "home.html";
-      }
-      
+    window.location.href = "home.html";
+  }
 }
 
 // localStorage - Allows you to save key-value pairs in the browser.
-function saveAccount(data){
-    localStorage.setItem(data.login, JSON.stringify(data)); // converts a JavaScript object into a JSON string
+function saveAccount(data) {
+  localStorage.setItem(data.login, JSON.stringify(data)); // converts a JavaScript object into a JSON string
 }
 
-function saveSession(data, saveSession){
-    if(saveSession){
-      localStorage.setItem("session", data);
-    }
+function saveSession(data, saveSession) {
+  if (saveSession) {
+    localStorage.setItem("session", data);
+  }
 
-    sessionStorage.setItem("logged", data);
+  sessionStorage.setItem("logged", data);
 }
 
-function getAccount(key){
-    const account = localStorage.getItem(key);
+function getAccount(key) {
+  const account = localStorage.getItem(key);
 
-    if(account){
-        return JSON.parse(account); // converts a JSON string into a JavaScript object
-    }
-    return "";
+  if (account) {
+    return JSON.parse(account); // converts a JSON string into a JavaScript object
+  }
+  return "";
 }
